4b1af7f2744743a7282be85828ca1f4ad87e8960
VinayDevarapu <108621826+VinayDevarapu@users.noreply.github.com>
2025-06-14 13:41:20 +0530

    updated1

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,7 +1,7 @@
 
 
 module "vpc" {
-  source       = "../.NETDEMO/modules/vpc"
+  source       = "../../modules/vpc"
   network_name = var.network_name
   subnet_name  = var.subnet_name
   subnet_range = var.subnet_range
@@ -9,20 +9,20 @@ module "vpc" {
 }
 
 module "sql_instance" {
-  source        = "../.NETDEMO/modules/sql"
+  source        = "../../modules/sql"
   instance_name = var.instance_name
   region        = var.region
   network       = module.vpc.network_name
 }
 
 module "secret_manager" {
-  source       = "../.NETDEMO/modules/secrets"
+  source       = "../../modules/secrets"
   secret_name  = var.secret_name
   secret_value = var.secret_value
 }
 
 module "vpc_connector" {
-  source         = "../.NETDEMO/modules/vpc_connector"
+  source         = "../../modules/vpc_connector"
   connector_name = var.connector_name
   region         = var.region
   network        = module.vpc.network_name
@@ -30,7 +30,7 @@ module "vpc_connector" {
 }
 
 module "cloud_run_service" {
-  source                  = "../.NETDEMO/modules/cloudrun"
+  source                  = "../../modules/cloudrun"
   service_name            = var.service_name
   image                   = var.image
   region                  = var.region
